2b779dce79d574c7c1345274c86742f87234a0fd
pewcia <ewelina.serafin996@gmail.com>
2025-04-17 19:04:13 +0200

    Added new ID in HTML and function to update city in Java Script

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+
+function updateCity (event){
+    let cityTimeZone=event.target.value;
+    let cityTime=moment().tz(cityTimeZone);
+    let citiesElement=document.querySelector(".cities");
+    let cityName=cityTimeZone.replace("_", "").split("/")[1];
+    citiesElement.innerHTML=
+   
+`
+<div class="row">
+        <div class="cityClock">
+      <span class="city" id="amsterdam"value="Europe/Amsterdam">${cityName} </span>
+      <span class="time"><strong> ${cityTime.format("HH:mm:ss")}</strong></span>
+      </div>
+      <span class="date">${cityTime.format("MMMM Do YYYY")}</span>
+   </div>`;
+
+}
+
+updateMadridTime();
+
+
+
 function updateMadridTime() {
 let madridElement=document.querySelector("#madrid");
 let madridRow = madridElement.closest(".row");
@@ -21,3 +44,18 @@ function updateAmsterdamTime() {
 }
 
 setInterval(updateAmsterdamTime, 1000);
+
+function updateWarsawTime() {
+  let warsawElement = document.querySelector("#warsaw");
+  let warsawRow = warsawElement.closest(".row");
+  let warsawDateElement = warsawRow.querySelector(".date");
+  let warsawTimeElement = warsawRow.querySelector(".time");
+  let warsawTime = moment().tz("Europe/Warsaw");
+  warsawDateElement.innerHTML = warsawTime.format("MMMM Do YYYY");
+  warsawTimeElement.innerHTML = warsawTime.format("HH:mm:ss ");
+}
+
+setInterval(updateWarsawTime, 1000);
+
+let citiesSelect = document.querySelector("#city");
+citiesSelect.addEventListener("change", updateCity);
